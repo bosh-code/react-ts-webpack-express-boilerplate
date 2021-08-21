27b5180126058e5192cdd54d89d46426f3f79906
ryanbosher <ryan.bosher@lancom.tech>
2021-08-21 12:01:53 +1200

    add redux-toolkit and reformat index to use it

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { applyMiddleware, compose, createStore } from 'redux'
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 
-const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const store = configureStore({
+    reducer: reducers,
+    });
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
